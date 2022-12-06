9b4d1182d0bb13de7c0e74829d38efc3de19a552
Henry Burbridge <burbridgeh1@gmail.com>
2022-12-06 17:07:13 +0000

    Added coments to daeomon.php

--- daemon/daemon.php
+++ daemon/daemon.php
@@ -80,6 +80,10 @@ function formatTime1($time) {
 
 function newId($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME, $con) {
 
+    // Function newId finds the highest SlotId currently stored into the database
+    // and adds 1 to it and returns the new SlotId
+    // This stops slots from having the same Id 
+
     $stmt = $con->prepare('SELECT MAX(SlotId) FROM `Slot`');
   
     $stmt->execute();
@@ -99,7 +103,7 @@ function newId($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME, $
 }
 
 function InsertVar($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME, $con, $Type, $SlotId, $TimeStart, $TimeFinish, $Date, $NumberUsers) {
-
+    // For helping debugging, will be removed soon
     echo 'Id: ' . $SlotId . "<br>" . "<br>"; 
 
     echo 'Slot Start'. $TimeStart . "<br>" . "<br>"; 
@@ -111,6 +115,8 @@ function InsertVar($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAM
     echo 'Date: ', $Date . "<br>" . "<br>"; 
 
     echo '---------------------------------------------' . "<br>"; 
+
+    // Below is where all the required variables for the slot are inserted into the database
    
     $stmt = $con->prepare('INSERT INTO `Slot`(`SlotId`, `TimeStart`, `TimeFinish`, `Date`, `NumberUsers`, `Type`) VALUES (?, ?, ?, ?, ?, ?) '); {
         
@@ -135,6 +141,12 @@ function createSlotDay($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE
         // Slot_finsih is 1 hour later than slot start
         
         // Insert values into table
+
+        // The 4 lines below increase both the start time for the slot
+        // and the end time for the slot
+        // The new times are also sent to the function fromattime1 which 
+        // formats them correctly to be inserted into the database 
+
     
         $TimeStart = $slot_start; 
     
@@ -166,24 +178,45 @@ function createSlotDay($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE
     
 
 }
+// New function addDate
+// Takes a date value as input and returns a date value
+// Variable $currentDate stores the date on which the loop is currently on
 
 function addDate($currentDate){
+    // This adds 1 day to the variable $currentDate and stores it in the variable $dateNew
     $dateNew = date('Y-m-d', strtotime($currentDate. ' + 1 days'));
 
+    // $dateNew is returned to be inserted into the slot
     return $dateNew;
 }
 
 function loopSlot($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME, $con, $hours_open, $time_open, $date){
+    
+    // Sets the current date
+    // This will be changed immediatley when the loops starts
+
     $dateNew = date("Y/m/d");
 
     for ($x = 1; $x <= 6; $x++) {
+
+        // Calls the function addDate and inputs the variable $dateNew
+        // $dateNew is the variable that stores the date which is also changed by the addDate function
+        // This allows it to continue to increase the date by 1 day as it loops 
+
         $dateNew = addDate($dateNew);
+
+        // Creates the slot
         createSlotDay($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME, $con, $hours_open, $time_open, $dateNew);
         
       }
 }
+// Creates the first slot for the current day
+// This is done before the loop starts as 1 day is immediatley added
+// Without doing this you would not create slots for the current day
 
 createSlotDay($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME, $con, $hours_open, $time_open, $date);
+
+// Rest of the 6 days of slots created in this function 
 loopSlot($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME, $con, $hours_open, $time_open, $date)
 
 
