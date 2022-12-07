2cce57668fef77b7b3e57451c07db8e22709c4cc
Henry Burbridge <burbridgeh1@gmail.com>
2022-12-07 08:56:27 +0000

    Time and slot type formatting

--- admin/adminHome.php
+++ admin/adminHome.php
@@ -7,6 +7,8 @@
         <link href="../styles/skeleton.css" rel="stylesheet" type="text/css">
 		<link href="../styles/style.css" rel="stylesheet" type="text/css">
 		<link href="../styles/normalize.css" rel="stylesheet" type="text/css"> 
+		<script src="jquery-3.6.1.min.js"></script>
+
 		
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
@@ -26,6 +28,24 @@
 </html>
 
 <?php
+function formatTime($time){
+	$time = substr($time, 0, -8);
+	return $time; 
+}
+
+function type($type){
+	if ($type == 0){
+		$Newtype = 'Cardio'; 
+		return $Newtype; 
+	} else {
+		if ($type == 1){
+			$Newtype = 'Weight'; 
+			return $Newtype; 
+		}
+
+	}
+}
+
 $con=mysqli_connect("localhost","root","","phplogin");
 // Check connection
 if (mysqli_connect_errno())
@@ -51,15 +71,17 @@ while($row = mysqli_fetch_array($result))
 
 echo "<tr>";
 echo "<td>" . $row['SlotId'] . "</td>";
-echo "<td>" . $row['TimeStart'] . "</td>";
-echo "<td>" . $row['TimeFinish'] . "</td>";
+echo "<td>" . formatTime($row['TimeStart']) . "</td>";
+echo "<td>" . formatTime($row['TimeFinish']) . "</td>";
 echo "<td>" . $row['Date'] . "</td>";
 echo "<td>" . $row['NumberUsers'] . "</td>";
-echo "<td>" . $row['Type'] . "</td>";
+echo "<td>" . type($row['Type']) . "</td>";
 echo "</tr>";
 }
 echo "</table>";
 
 mysqli_close($con);
+
+
 
 ?>
