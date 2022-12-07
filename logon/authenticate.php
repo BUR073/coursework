24b613c6a6dcfe6476d419a16ecc9016514fcb8a
Henry Burbridge <burbridgeh1@gmail.com>
2022-12-07 10:03:47 +0000

    comment

--- logon/authenticate.php
+++ logon/authenticate.php
@@ -20,14 +20,16 @@ if ( mysqli_connect_errno() ) {
     
 }
 
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+// Checks if details have been entered fully in html form. I either fields
+// are empty then user will be warned and returned to login
 if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
 	
     echo "<script>alert('Please fill both the username and password fields');document.location='index.html'</script>";
 }
 
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+// Find users details from database. UserID, password and admin are retrieved 
+// These details are them binded to the varaibles $id, $password and $admin
 if ($stmt = $con->prepare('SELECT UserId, password, admin FROM User WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
@@ -41,13 +43,26 @@ if ($stmt = $con->prepare('SELECT UserId, password, admin FROM User WHERE userna
         // Account exists, now we verify the password.
 
         if ($_POST['password'] === $password) {
-            // Verification success! User has logged-in!
-            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            // Checks that the password matches the password entered
             session_regenerate_id();
+            // Create new session id
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
             $_SESSION['id'] = $id;
-            header('Location: ../home/home.php');
+
+            // Store variables in session
+            // This is usefule later for booking, negates the need for entering all of 
+            // the users details into the html form as most can be found from the session
+            // Also stops users who are not logged in from accessing pages behind the
+            // login. 
+
+            // header('Location: ../home/home.php');
+
+            // Takes user to home page
+            
+            // Check if user has admin rights
+            // If they do send them to the admin homepage
+            // Otherwise send them to the standard homepage
 
             if ($admin == 0) {
                 header('Location: ../home/home.php');
@@ -59,11 +74,13 @@ if ($stmt = $con->prepare('SELECT UserId, password, admin FROM User WHERE userna
 
 
         } else {
-            // Incorrect password
+            // If password is not correct
+            // Warn user and send them back to login
             echo "<script>alert('Incorrect username and/or password');document.location='index.html'</script>";
         }
     } else {
-        // Incorrect username
+        // Same for if username is correct
+        // Same message and action for user
         echo "<script>alert('Incorrect username and/or password');document.location='index.html'</script>";
     }
 
